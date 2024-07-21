67d3498ad5134a80b362047e04bf455e28c75888
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2024-07-21 23:56:35 +0200

    Add home page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { CssBaseline } from '@mui/material';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <p>Hello, world!</p>
+      <Home />
+      
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Home() {
+    return <Container>
+        <Typography variant="h3" gutterBottom>Bonjour 👋</Typography>
+        <Typography variant="body1">I'm Antoine, and I've been a software engineer
+            for 8 years.
+        </Typography>
+    </Container>;
+}

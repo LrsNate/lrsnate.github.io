b13057d93c16f2a86629717538d2667092522756
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2024-07-27 12:12:12 +0200

    Copy content from CRA site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
+import { Container, Stack } from "@mui/material";
+import Bio from "./sections/Bio";
+import WorkExperience from "./sections/WorkExperience";
 
 export default function Home() {
-  return <p>Hello world!</p>;
+  return (
+    <Container maxWidth="md">
+      <Stack spacing={16}>
+        <Bio />
+        <WorkExperience />
+      </Stack>
+    </Container>
+  );
 }
